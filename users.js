e6fab33ee6abc3f302a8592429a2858e8da0660b
alessandrocaseti <90320229+alessandrocaseti@users.noreply.github.com>
2025-06-28 20:39:44 +0200

    feature: implement users system (part 1)
    Introduced user management features including user creation, avatar selection, and deletion. Added new user interface elements in index.html, supporting styles in style.css, user avatar assets, and implemented the logic in users.js. This enables toggling user experience, managing up to 4 users, and selecting from 8 avatars.

---
+++ users.js
@@ -0,0 +1,122 @@
+//// GAMES HUB ////
+
+var currentUserID = null;
+
+var avatar = ["assets/nouser.png", "assets/nouser.png", "assets/nouser.png", "assets/nouser.png"];
+var XP = [0, 0, 0, 0];
+var username = ["1", "2", "3", "4"];
+
+function toggleUsers() 
+{
+  playMainClick();
+
+  const setting = document.getElementById("usersToggle");
+  if (setting.checked)
+  {
+    currentUserID = -1;
+    document.getElementById("addUserCard").style.display = "grid";
+    document.getElementById("appendChildDiv").style.display = "block";
+  }
+  else 
+  {
+    currentUserID = null;
+    document.getElementById("addUserCard").style.display = "none";
+    document.getElementById("appendChildDiv").style.display = "none";
+
+    for (let i = 0; i < 4; i++) 
+    {
+      deleteUser(i, true);
+    }
+  }
+}
+
+function openUserDialog()
+{
+  console.log("currentUSERID: " + currentUserID)
+  playMainClick();
+
+  if (currentUserID === 3) 
+  {
+    alert("You can only add up to 4 users.");
+    return;
+  }
+
+  currentUserID++;
+
+  for (let i = 1; i <= 8; i++) 
+  {
+    document.getElementById("avatar" + i).style.backgroundImage = "url('assets/user" + i + ".png')";
+  }
+
+  document.getElementById("box").style.display = "grid";
+}
+
+function closeUserDialog()
+{
+  playMainClick();
+  document.getElementById("box").style.display = "none";
+  document.getElementById("newUserName").value = "";
+}
+
+function selectAvatar(avatarId)
+{
+  playMainClick();
+
+  for (let i = 1; i <= 8; i++) 
+  {
+    document.getElementById("avatar" + i).style.borderColor = "white";
+  }
+
+  document.getElementById("avatar" + avatarId).style.borderColor = "yellow"; // Highlight selected avatar
+
+  avatar[currentUserID] = 'assets/user' + avatarId + '.png'; // STORE selected avatar
+}
+
+function addUser() 
+{
+  const userName = document.getElementById("newUserName").value;
+  if (userName === "") 
+  {
+    alert("Please enter a valid username!");
+    return;
+  }
+
+  // USER CARD ATTRIBUTES
+  document.getElementById("user" + currentUserID).style.display = "grid";
+  document.getElementById("userName" + currentUserID).innerText = userName;
+  document.getElementById("userXP" + currentUserID).innerText = "XP: " + XP[currentUserID];
+  document.getElementById("userPic" + currentUserID).src = avatar[currentUserID];
+
+  username[currentUserID] = userName; // STORE new username
+
+  console.log("ADDED USER " + currentUserID);
+  console.log("username " + username[currentUserID] + " pic: " + avatar[currentUserID])
+
+  closeUserDialog();
+}
+
+function deleteUser(id, triggerSound) 
+{
+  if(!triggerSound)
+  {
+    playMainClick();
+  }
+
+  document.getElementById("user" + id).style.display = "none";
+  document.getElementById("userName" + id).innerText = "";
+  document.getElementById("userXP" + id).innerText = "";
+
+  currentUserID--;
+
+  username[id] = "";
+  avatar[id] = "assets/nouser.png";
+  XP[id] = 0;
+}
+
+function debug()
+{
+  for (let i = 0; i < 4; i++) {
+    console.log("User " + i + ": " + username[i] + ", Avatar: " + avatar[i] + ", XP: " + XP[i]);
+  }
+}
+
